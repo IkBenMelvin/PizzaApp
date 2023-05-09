546b29f1c2d2d8b43b2580a7b4babb7e09293270
killbyfire <6029537@mborijnland.nl>
2023-05-09 13:02:39 +0200

    Got to fix item list grid system

--- App.js
+++ App.js
@@ -2,10 +2,14 @@ import React from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { SafeAreaView, StyleSheet, Text, View, Button, Alert, TextInput } from 'react-native';
 import Featured from './components/Featured'
+import ItemList from './components/itemList';
 
 export default function App() {
   return (
-    <Featured />
+    <>
+      <Featured />
+      <ItemList />
+    </>
     // <View style={styles.container}>
     //   <Text style={{color: '#fff', fontSize:30}}>Hello Fam</Text>
     //   <StatusBar style="auto" />
--- components/itemList.jsx
+++ components/itemList.jsx
@@ -2,10 +2,48 @@ import React from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { SafeAreaView, StyleSheet, Text, View, Button, Alert, TextInput, Image, Pressable } from 'react-native';
 
-export default function itemList() {
+export default function ItemList() {
+    const [pizzas, setPizzas] = React.useState([{id: 1, name: "Margerita", ingredients: [], price: 20, img: "placeholder.jpg"},]);
     return (
     <View>
-      <Text>Item List</Text>
+      <Text style={styles.allItemsText}>Choose from all pizzas:</Text>
+        <View style={styles.pizzaContainer}>
+            {pizzas.map(pizza => 
+                <View key={pizza.id} style={styles.pizzaCard}>
+                    <Image source={require(`../assets/images/placeholder.jpg`)} style={styles.pizzaImage}></Image>
+                    <Text>{pizza.name}</Text>
+                    <View style={styles.pizzaIngredients}>
+                    </View>
+                    <Text>{pizza.price}</Text>
+                </View>
+            )}
+      </View>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+    allItemsText: {
+        marginTop: 20,
+        marginLeft: 15,
+        fontSize: 30,
+        fontWeight: 'bold',
+    },
+    pizzaContainer: {
+        flex: 2,
+        width: '100%',
+        height: '100%',
+    },
+    pizzaCard: {
+        // width: '30%',
+        // height: '15%',
+        width: '45%',
+        height: '60%',
+        backgroundColor: '#ddd'
+    },
+    pizzaImage: {
+        width: '100%',
+        height: '75%',
+
+    }
+});
