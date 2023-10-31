37b99d8cc9f01135d1edcc5bcf04bbd4d017250e
killbyfire <6029537@mborijnland.nl>
2023-10-31 12:10:09 +0100

    Exports/import cleanup

--- App.js
+++ App.js
@@ -3,26 +3,7 @@ import { StatusBar } from 'expo-status-bar';
 import { SafeAreaView, StyleSheet, Text, View, Button, Alert, TextInput, ScrollView } from 'react-native';
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
-// Main screens
-import HomePage from './screens/homePage';
-import LoginPage from './screens/loginPage';
-import RegisterPage from './screens/registerPage';
-import LogoutPage from './screens/logoutPage';
-import CartScreen from './screens/cartPage';
-import PizzaAdjust from './screens/pizzaAdjust';
-import UserOrders from './screens/userOrders';
-// Admin
-import UploadPage from './screens/admin/addPizza';
-import ProductPage from './screens/admin/allProducts';
-import OrderPage from './screens/admin/allOrders';
-import UserPage from './screens/admin/allUsers';
-import PizzaList from './screens/admin/pizzaList';
-import EditPizzaPage from './screens/admin/editPizza';
-import DeletePizzaPage from './screens/admin/deletePizza';
-import EditOrderPage from './screens/admin/editOrder';
-import DeleteOrderPage from './screens/admin/deleteOrder';
-import SpecialPage from './screens/specialPage';
-import Dashboard from './screens/admin/dashboard';
+import { HomePage, LoginPage, RegisterPage, LogoutPage, CartScreen, PizzaAdjust, UserOrders, UploadPage, ProductPage, OrderPage, UserPage, PizzaList, EditPizzaPage, DeletePizzaPage, EditOrderPage, DeleteOrderPage, SpecialPage, Dashboard } from './screens';
 
 const Stack = createNativeStackNavigator();
 
---
+++ screens.js
@@ -0,0 +1,22 @@
+// Main screens
+import HomePage from './screens/homePage';
+import LoginPage from './screens/loginPage';
+import RegisterPage from './screens/registerPage';
+import LogoutPage from './screens/logoutPage';
+import CartScreen from './screens/cartPage';
+import PizzaAdjust from './screens/pizzaAdjust';
+import UserOrders from './screens/userOrders';
+// Admin
+import UploadPage from './screens/admin/addPizza';
+import ProductPage from './screens/admin/allProducts';
+import OrderPage from './screens/admin/allOrders';
+import UserPage from './screens/admin/allUsers';
+import PizzaList from './screens/admin/pizzaList';
+import EditPizzaPage from './screens/admin/editPizza';
+import DeletePizzaPage from './screens/admin/deletePizza';
+import EditOrderPage from './screens/admin/editOrder';
+import DeleteOrderPage from './screens/admin/deleteOrder';
+import SpecialPage from './screens/specialPage';
+import Dashboard from './screens/admin/dashboard';
+
+export { HomePage, LoginPage, RegisterPage, LogoutPage, CartScreen, PizzaAdjust, UserOrders, UploadPage, ProductPage, OrderPage, UserPage, PizzaList, EditPizzaPage, DeletePizzaPage, EditOrderPage, DeleteOrderPage, SpecialPage, Dashboard}
